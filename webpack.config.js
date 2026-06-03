5c589ac37cdbd715091d55a5f236d89d319a6c12
Thien Thien <thanhoangthienthien@gmail.com>
2026-06-03 17:09:30 +0700

    chore: update build output directory from dist to build in vercel.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const stylesHandler = MiniCssExtractPlugin.loader;
 const config = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build"),
     },
     resolve: {
         extensions: [".js", ".jsx", ".json", ".css"],
